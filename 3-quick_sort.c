107dc67bfbadfd1be1a57300125befa00bcd159e
deking254 <dekingsky@gmail.com>
2023-06-27 16:21:59 +0000

    all

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,79 +1,80 @@
 #include "sort.h"
+#include <stdio.h>
 #include <sys/types.h>
+void switcher(int *array, ssize_t i, ssize_t size);
+void q_sort(int *array, ssize_t first, ssize_t last, int size);
+int partition(int *array, ssize_t first, ssize_t last, size_t size);
 /**
-*swap - the positions of two elements into an array
-*@array: array
-*@item1: array element
-*@item2: array element
-*/
-void swap(int *array, ssize_t item1, ssize_t item2)
-{
-	int tmp;
-
-	tmp = array[item1];
-	array[item1] = array[item2];
-	array[item2] = tmp;
-}
-/**
- *lomuto_partition - lomuto partition sorting scheme implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: size array
- *Return: return the position of the last element sorted
- */
-int lomuto_partition(int *array, ssize_t first, ssize_t last, size_t size)
-{
-	int pivot = array[last];
-	ssize_t current = first, finder;
-
-	for (finder = first; finder < last; finder++)
-	{
-		if (array[finder] < pivot)
-		{
-			if (array[current] != array[finder])
-			{
-				swap(array, current, finder);
-				print_array(array, size);
-			}
-			current++;
-		}
-	}
-	if (array[current] != array[last])
-	{
-		swap(array, current, last);
-		print_array(array, size);
-	}
-	return (current);
-}
-/**
- *qs - qucksort algorithm implementation
- *@array: array
- *@first: first array element
- *@last: last array element
- *@size: array size
- */
-void qs(int *array, ssize_t first, ssize_t last, int size)
-{
-	ssize_t position = 0;
-
-
-	if (first < last)
-	{
-		position = lomuto_partition(array, first, last, size);
-
-		qs(array, first, position - 1, size);
-		qs(array, position + 1, last, size);
-	}
-}
-/**
- *quick_sort - prepare the terrain to quicksort algorithm
+ *quick_sort - check code
  *@array: array
  *@size: array size
  */
 void quick_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
-	qs(array, 0, size - 1, size);
+if (array == NULL || size < 2)
+return;
+q_sort(array, 0, size - 1, size);
+}
+/**
+ * switcher - Entry point
+ * @array: array
+ * @i: i
+ * @size: size
+ * Return: Always 0
+ */
+void switcher(int *array, ssize_t item1, ssize_t item2)
+{
+int tmp;
+tmp = array[item1];
+array[item1] = array[item2];
+array[item2] = tmp;
+}
+/**
+ *partition - lomuto
+ *@array: array
+ *@init: first array element
+ *@last: last array element
+ *@size: size array
+ *Return: return the position of the last element sorted
+ */
+int partition(int *array, ssize_t init, ssize_t last, size_t size)
+{
+int piv = array[last];
+ssize_t current = init, finder;
+for (finder = init; finder < last; finder++)
+{
+if (array[finder] < piv)
+{
+if (array[current] != array[finder])
+{
+switcher(array, current, finder);
+print_array(array, size);
+}
+current++;
+}
+}
+if (array[current] != array[last])
+{
+switcher(array, current, last);
+print_array(array, size);
+}
+return (current);
+}
+
+/**
+ *q_sort - check code
+ *@array: array
+ *@first: first array element
+ *@last: last array element
+ *@size: array size
+ */
+void q_sort(int *array, ssize_t first, ssize_t last, int size)
+{
+ssize_t pos = 0;
+if (first < last)
+{
+pos = partition(array, first, last, size);
+q_sort(array, first, pos - 1, size);
+q_sort(array, pos + 1, last, size);
+}
 }
